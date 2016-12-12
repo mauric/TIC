ac87a7828f12acb6a968e0d21cb1b383302d8227
mauric <mauric.caceres@gmail.com>
2016-12-12 09:09:43 +0100

    adding the firts program

---
+++ optmisation_canal.m
@@ -0,0 +1,10 @@
+%% TP TIC
+% Codage de source et optimisation de la capacité du canal
+
+% Ecrire une fonction Matlab permettant d’engendrer une séquence binaire (pseudo) aléatoire
+% { } de longueur
+%  paramétrable. Cette fonction prendra également en entrée le
+% paramètre =
+%  =0 .
+
+N = 
