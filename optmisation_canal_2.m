ba0236d95c5485d73cd2c15e7947a85db435246c
mauric <mauric.caceres@gmail.com>
2016-12-20 13:23:46 +0100

    code

--- optmisation_canal_2.m
+++ optmisation_canal_2.m
@@ -8,8 +8,8 @@ close all
 
 %% Parametres 
 p1 = 0.1;    % probabilites de transition 
-p2 = 0.2;    % probabilites de transition
-N = 1e6; % valuer pour lequel on obtien une bonne courbe
+p2 = 0.1;    % probabilites de transition
+N = 1e3; % valuer pour lequel on obtien une bonne courbe
 %N = 6; % pour tester le sequences 
 
 %% boucle principal - variation de alpha
@@ -44,35 +44,16 @@ IXY(i) = HB - HAB;
 
 end
 
-Capacite_experimental = max(IXY)
-
-A1 = -p1*log2(p1) - (1-p1)*log2(1-p1)
-A2 = -p2*log2(p2) - (1-p2)*log2(1-p2)
-
-%%Calcul de alpha optimale theorique
-L = 2^((A1-A2)/(1-p1-p2))
-alphaopt = (1-p2*(1+L))/((1-p1-p2)*(1+L))
-
-%%calcul de la capacite
-K = alphaopt*(1-p1)+(1-alphaopt)*p2
-HBm = -K*log2(K)-(1-K)*log2(1-K)
-HABm = A1*alphaopt+A2*(1-alphaopt)
-Capacite_theorique = HBm - HABm
-
-
 
 %% GRAPHICS
 figure(1)
 plot(alpha,IXY);
 hold on 
 plot(alpha,IXYest,'r');
-title('Infomation Mutuelle estimé et theorique','FontSize',12);
-xlabel('\alpha','FontSize',12);
-ylabel('IXY(\alpha)','FontSize',12);
-
 E = (IXY - IXYest);
+
 figure(2)
-errorbar(IXY(1,1:4:discretisation),30*E(1,1:4:discretisation))
+errorbar(IXY(1,1:4:discretisation),5*E(1,1:4:discretisation))
 grid()
 title('Erreur entre courbes (avec facteur d echelle)','FontSize',12);
 xlabel('i','FontSize',12);
@@ -98,10 +79,6 @@ ylabel('IXY(\alpha)','FontSize',12);
 
 
 
-
-
-
-
 %% TEST BSC
 % figure()
 % plot(alpha,IXYbsc,'b');
@@ -112,5 +89,5 @@ ylabel('IXY(\alpha)','FontSize',12);
 % sauvegarde les images pour le rapport
 h = get(0,'children');
 for i=length(h):-1:1
-  saveas(h(i), ['canal' num2str(length(h)+1-i)], 'png');
+  saveas(h(i), ['canal2' num2str(length(h)+1-i)], 'png');
 end
